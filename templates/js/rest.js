3d3b0be46b4aef4a0d4b37f3de9e5130ccc84f4a
70_10 <chiclown.dev@gmail.com>
2016-02-13 16:17:34 +0900

    [add] rest

---
+++ templates/js/rest.js
@@ -0,0 +1,44 @@
+$(function() {
+
+  var interval = setInterval(function () {
+    var current = parseInt($(".condara-time").text());
+    if (current > 0) {
+      var time = current - 1;
+      console.log(time);
+      $(".condara-time").text(time);
+      $(".condara-time-str").text(toHms(time));
+      if (time == 0) {
+        $(".condara-button").removeClass("is-disabled");
+      }
+    } else {
+      clearInterval(interval);
+    }
+  }, 1000);
+
+
+  function toHms(t) {
+    var hms = "";
+    var h = t / 3600 | 0;
+    var m = t % 3600 / 60 | 0;
+    var s = t % 60;
+
+    if (h != 0) {
+      hms = h + "時間" + padZero(m) + "分" + padZero(s) + "秒";
+    } else if (m != 0) {
+      hms = m + "分" + padZero(s) + "秒";
+    } else {
+      hms = s + "秒";
+    }
+
+    return hms;
+
+    function padZero(v) {
+      if (v < 10) {
+        return "0" + v;
+      } else {
+        return v;
+      }
+    }
+  }
+
+});
