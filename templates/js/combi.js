d66732262e480e9a6fa366a8675c7a4ba5af9f68
70_10 <chiclown.dev@gmail.com>
2016-02-14 01:50:05 +0900

    [mod] combi count

--- templates/js/combi.js
+++ templates/js/combi.js
@@ -1,9 +1,32 @@
 $(function() {
-  setTimeout(function () {
-    pushableButton();
-  }, 10);
+  var wordAllCount = parseInt($(".condara-count").text());
 
   function pushableButton() {
     $(".combi-button").removeClass("is-disabled").addClass("is-danger");
+  }
+
+  $(".condara-idea").each(function(){
+    $(this).bind("keyup", hoge(this));
+  });
+  function hoge(elm){
+    var v, old = elm.value;
+    return function(){
+      if(old != (v=elm.value)){
+        var containsIdea = false;
+        var ideaCount = 0;
+        $(".condara-idea").each(function() {
+          if ($(this).val() !== "") {
+            containsIdea = true;
+            ideaCount++;
+          }
+        });
+        $(".condara-count").text(wordAllCount - ideaCount);
+        if (containsIdea) {
+          pushableButton();
+        } else {
+          $(".combi-button").removeClass("is-danger").addClass("is-disabled");
+        }
+      }
+    };
   }
 });
--- templates/js/inspi.js
+++ templates/js/inspi.js
@@ -12,26 +12,6 @@ $(function() {
     $(".condara-textarea").val(wordToInspi[$(this).val()]);
   });
 
-  // $(".condara-textarea").change(function() {
-  //   var word = $(".condara-select").val();
-  //   console.log(word);
-  //   wordToInspi[word] = $(this).text();
-  //   console.log(wordToInspi);
-  // });
-  // $(".condara-textarea").keypress(function() {
-  //   console.log("keypress");
-  //   var word = $(".condara-select").val();
-  //   wordToInspi[word] = $(this).val();
-  // });
-  // // deleteキーとbackspaceキーの入力を検知
-  // $(".condara-textarea").keyup(function(e) {
-  //   if (e.keyCode == 46 || e.keyCode == 8){
-  //     console.log("keyup");
-  //     var word = $(".condara-select").val();
-  //     wordToInspi[word] = $(this).val();
-  //   }
-  // });
-  //
   $(".condara-textarea").each(function(){
     $(this).bind("keyup", hoge(this));
   });
