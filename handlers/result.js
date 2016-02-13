315b3c5ee7a0bce17fe5c1908e0a3a0752e8e591
17P <inagaki_mo@mti.co.jp>
2016-02-13 17:13:02 +0900

    [mod] エラーハンドリング
    postdataがないときはエラーとする

--- handlers/result.js
+++ handlers/result.js
@@ -1,8 +1,6 @@
 module.exports = (request, reply) => {
   reply.view("result", {
     title: "examples/views/jade/index.js | Hapi " + request.server.version,
-    main_theme: request.payload.main_theme,
-    sub_theme: request.payload.sub_theme,
-    inspiration: request.payload.inspiration
+    postData: request.payload
   });
 };
