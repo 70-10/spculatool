7b37f1392e42c5ea5d8c3de99252be7a9e71ab51
17P <inagaki_mo@mti.co.jp>
2016-02-13 16:54:49 +0900

    [add] POSTで受け取ったデータを表示する結果画面作成

--- handlers/result.js
+++ handlers/result.js
@@ -1,6 +1,8 @@
 module.exports = (request, reply) => {
   reply.view("result", {
     title: "examples/views/jade/index.js | Hapi " + request.server.version,
-    message: "About - Hello World!"
+    main_theme: request.payload.main_theme,
+    sub_theme: request.payload.sub_theme,
+    inspiration: request.payload.inspiration
   });
 };
