1bbbfe6a044a654b25dd87f810782502dc70a767
70_10 <chiclown.dev@gmail.com>
2016-02-13 22:23:46 +0900

    [mod] redirect

--- handlers/result.js
+++ handlers/result.js
@@ -1,11 +1,21 @@
+const Database = require("../database");
+
 module.exports = (request, reply) => {
 
-  if (request.payload && 'sub_theme' in request.payload && request.payload.sub_theme === 'fuga') {
-    reply.view("combi", {
-      main_theme: "メインテーマ",
-      words: request.payload.words,
-      error_message: "残念! 既存のアイデアです。練り直してください。"
+  if (request.payload && "sub_theme" in request.payload && request.payload.sub_theme === "料理") {
+    console.log(request.payload);
+    Database.findAllWord(result => {
+      reply.view("combi", {
+        main_theme: request.payload.main_theme,
+        words: result,
+        error_message: "残念! 既存のアイデアです。練り直してください。",
+      });
     });
+    // reply.view("combi", {
+    //   main_theme: "メインテーマ",
+    //   words: request.payload.words,
+    // });
+
   } else {
     reply.view("result", {
       title: "examples/views/jade/index.js | Hapi " + request.server.version,
